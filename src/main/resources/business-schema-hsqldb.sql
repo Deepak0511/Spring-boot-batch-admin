39cace5f84de43deb6f3dd08d659cce79debe4af
Deepak Chaudhary <deepak.kr@live.com>
2019-02-17 22:31:35 +0530

    first commit

---
+++ src/main/resources/business-schema-hsqldb.sql
@@ -0,0 +1,19 @@
+--select 1 from information_schema.TABLE_NAME;
+CREATE SEQUENCE IF NOT EXISTS BATCH_JOB_EXECUTION_SEQ START WITH 1;
+CREATE SEQUENCE IF NOT EXISTS BATCH_JOB_SEQ START WITH 1;
+CREATE SEQUENCE IF NOT EXISTS BATCH_STEP_EXECUTION_SEQ START WITH 1;
+
+
+
+--select * from BAtch_job_execution ;
+--select * from Batch_job_execution_context ;
+--select * from Batch_job_execution_params ;
+--select * from Batch_job_execution_seq ;
+--select * from Batch_job_instance ;
+--select * from Batch_job_seq ;
+--select * from Batch_step_execution ;
+--select * from Batch_step_execution_context ;
+--select * from Batch_step_execution_seq ;
+
+
+--select sysdate from dual;
